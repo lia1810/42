df97c77ecf58e72a54d1d1f809f9bf297a197bbf
unknown <lbelliski@gmail.com>
2019-11-19 23:48:26 -0300

    first commit

---
+++ 42.js
@@ -0,0 +1,90 @@
+// let nombre = prompt("Tu nombre es ")
+// console.log (nombre.length);
+// let nombre = "Liliana"
+// let apellido = "Belliski"
+// console.log ( `Mi nombre es ${nombre} y tiene ${nombre.length} letras`);
+// let texto = "HOLA MUNDO"
+// console.log (texto.toLowerCase ())
+//  let texto = "hola, mundo"
+//  console.log (texto.toUpperCase ())
+
+//ejercicio 43
+// let texto = "the dark knight"
+// let primeraLetra = texto.charAt(0)
+// let segundaLetra = texto.charAt(4)
+// let tercerLetra = texto.charAt(9)
+// let resultado = primeraLetra.concat(segundaLetra).concat(tercerLetra)
+// console.log(resultado.toUpperCase())
+
+
+//ejercicio 44 funciona
+// let texto = "HOLA"
+// let letra4 = texto.charAt(3)
+// let letra3 = texto.charAt(2)
+// let letra2 = texto.charAt(1)
+// let letra1 = texto.charAt(0)
+// let resultado = letra4.concat(letra3).concat(letra2).concat(letra1)
+// console.log(resultado.toLowerCase())
+
+//ejercicio 44 que no funciona pero debería
+// let texto="HOLA"
+// let resultado = texto.charAt(3).concat().texto.charAt(2).concat().texto.charAt(1).concat().texto.charAt(0)
+// console.log (resultado.toLowerCase())
+
+
+
+
+// let alumnas = ["Lili", "Maria", "Nil", "Sabri", "Sol", "Tefi"];
+
+// for (let i = 0; i < alumnas.length; i++) {
+//    let nombreEnMinusculas= alumnas[i].toLowerCase();
+//     let primeraLetra = nombreEnMinusculas.charAt(0).toUpperCase();
+//     let restoDelNombre = nombreEnMinusculas.slice(1, nombreEnMinusculas.length)
+//     console.log(primeraLetra.concat(restoDelNombre))
+
+
+// }
+// ejercicio 1
+// let playlistFoo = ['Everlong', 'The Pretender', 'Learn to Fly', 'Best of You', 'All My Life', 'Monkey Wrench', 'The Sky is a Neighborhood'];
+// for (let i = 0; i < playlistFoo.length; i++) {
+
+//     console.log (`La segunda cancion es ${playlistFoo[1]} y 
+//         la quinta es ${ playlistFoo[4]}`)
+
+// }
+
+// ejercicio 5
+// let alumnas =[]
+// for (let i = 0; i < alumnas.length; i++) {
+
+//     console.log ( 
+//     )
+
+// }
+// ejercicio 8
+// let etiquetasHtml = [`header`, `form`, `footer`, `button`, `link`]
+
+// for (let i = 0; i < etiquetasHtml.length; i++) {
+//     const element = etiquetasHtml;
+//     console.log(element[1].toUpperCase(), element[4].toLowerCase())
+
+// }
+
+// ejercicio 9
+// let ganadorasRupaul = ['BeBe Zahara Benet',
+//  'Tyra Sanchez', 
+//  'Raja', 
+//  'Sharon Needles', 
+//  'Jinkx Monsoon', 
+//  'Bianca del Rio', 
+//  'Violet Chachki', 
+//  'Bob the Drag Queen', 
+//  'Sasha Velour', 
+//  'Aquaria'];
+//  for (let i = 0; i < ganadorasRupaul.length; i++) {
+//      const element = ganadorasRupaul;
+//      console.log( element[0],
+//         element[element.length-1]
+//         )
+
+//  }
